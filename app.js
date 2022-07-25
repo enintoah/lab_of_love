959506c4456fef68aafbb68c90e8e7d5d502c0d8
Anthonie Lorsithong <anthoniecorps@gmail.com>
2022-07-24 20:32:37 -0700

    first commit

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ const express = require("express");
 const mongoose = require('mongoose');
 const app = express();
 const db = require('./config/keys').mongoURI;
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const users = require("./routes/api/users");
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
-app.get("/", (req, res) => res.send("Hello World"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use("/api/users", users);
+app.use(passport.initialize());
+require('./config/passport')(passport);
 const port = process.env.PORT || 5050;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
